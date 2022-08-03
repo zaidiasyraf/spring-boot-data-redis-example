ebbf7ebba29075edc73e233acfe7312a82c87122
zaidi <zaidi.asyraf@yahoo.com>
2022-08-03 09:36:27 +0800

    Add spring cache example

--- src/main/java/com/zaidi/springdataredisexample/SpringDataRedisExampleApplication.java
+++ src/main/java/com/zaidi/springdataredisexample/SpringDataRedisExampleApplication.java
@@ -2,11 +2,13 @@ package com.zaidi.springdataredisexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringDataRedisExampleApplication {
 
 	public static void main(String[] args) {
